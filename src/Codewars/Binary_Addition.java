652f4f243fb52e26ea55f71830b8bbeb81bc4271
sklyarkostya <71768973+sklyarkostya@users.noreply.github.com>
2023-06-12 23:04:38 +0500

    timus and codewars

---
+++ src/Codewars/Binary_Addition.java
@@ -0,0 +1,8 @@
+package Codewars;
+
+public class Binary_Addition {
+    public static String binaryAddition(int a, int b){
+        //your code here
+        return Integer.toBinaryString(a + b);
+    }
+}
---
+++ src/timus/Task1053.java
@@ -0,0 +1,18 @@
+package timus;
+
+import java.util.Scanner;
+
+public class Task1053 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long gcd = in.nextLong();
+        for(int i = 1; i < n; i++)
+            gcd = gcd(gcd, in.nextLong());
+        System.out.println(gcd);
+    }
+    public static long gcd(long x,long y) {
+        if(x == 0) return y;
+        return gcd(y % x, x);
+    }
+}
